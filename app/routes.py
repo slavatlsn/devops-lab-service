2c5b373b4dec703a8ea4e7b31f41b23358b54a50
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2026-05-18 17:27:00 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ task_id = 0
 
 @router.get("/")
 def root():
-    return {"message": "Service is running. Hello!"}
+    return {"message": "Service is running. Hello, world!"}
 
 
 @router.post("/tasks")
